e2ab6fd9090047ce0068a2312f3f0290805d359f
Leesanchez <anthony.l.sanchez@gmail.com>
2024-10-11 14:16:16 +0200

    Visualizations

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <string>
 #include <vector>
 #include "src/get_data.h"
-#include "src/planets.h"  
+#include "src/planets.h"
 #include <vector>
 #include <cmath>
 #include <cstdlib>
@@ -13,12 +13,30 @@
 using namespace std;
 
 // Constants for scaling and positioning
-const double EARTH_RADIUS = 6371.0;  // Earth radius in kilometers
-const double SCALE_FACTOR = 0.00001;  // Reduced scaling factor for visualization
+const double EARTH_RADIUS = 6371.0;
+const double SCALE_FACTOR = 0.00001;
 const float WINDOW_CENTER_X = 400;
 const float WINDOW_CENTER_Y = 400;
-float timeElapsed = 0.0f;  // Time-scrolling variable
-bool draggingSlider = false;  // Slider drag state
+float timeElapsed = 0.0f;
+bool draggingSlider = false;
+
+// Function to create and manage the slider
+void drawSlider(sf::RenderWindow& window, sf::RectangleShape& slider, sf::RectangleShape& handle, float& timeElapsed) {
+    // Event handling for dragging the slider
+    if (draggingSlider) {
+        sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+        float newPosX = mousePos.x - handle.getSize().x / 2;
+        if (newPosX >= slider.getPosition().x && newPosX <= slider.getPosition().x + slider.getSize().x - handle.getSize().x) {
+            handle.setPosition(newPosX, handle.getPosition().y);
+            // Update time based on slider position (normalized between 0 and 1)
+            timeElapsed = (handle.getPosition().x - slider.getPosition().x) / (slider.getSize().x - handle.getSize().x);
+        }
+    }
+
+    // Draw slider and handle
+    window.draw(slider);
+    window.draw(handle);
+}
 
 // SpaceBody Class
 class SpaceBody {
@@ -30,14 +48,12 @@ public:
         cout << "Name: " << name << ", Diameter: " << diameter << " km, Mass: " << mass << " kg" << endl;
     }
 
-    // Calculate surface gravity
     double calculateSurfaceGravity() const {
         const double G = 6.67430e-11;
         double radius_m = (diameter * 1000) / 2.0;
         return (G * mass) / (radius_m * radius_m);
     }
 
-    // Calculate escape velocity
     double calculateEscapeVelocity() const {
         const double G = 6.67430e-11;
         double radiusMeters = (diameter * 1000) / 2.0;
@@ -45,25 +61,53 @@ public:
         return escapeVelocity_m_s / 1000.0;
     }
 
-    // Getter for mass and diameter
     double getMass() const { return mass; }
     double getDiameter() const { return diameter; }
 
-    // Destructor
     virtual ~SpaceBody() {
         cout << "Space body " << name << " memory freed." << endl;
     }
 
 protected:
     string name;
-    double diameter;  
-    double mass;      
+    double diameter;
+    double mass;
 };
 
-// Derived class for Asteroids
+// Planet Class
+class Planet : public SpaceBody {
+public:
+    Planet(const string& name, double diameter, double mass)
+        : SpaceBody(name, diameter, mass) {}
+
+    void printInfo() const override {
+        cout << "Planet Name: " << name << ", Mass: " << mass << " kg, Diameter: " << diameter << " km" << endl;
+        cout << "Surface Gravity: " << calculateSurfaceGravity() << " m/s^2" << endl;
+        cout << "Escape Velocity: " << calculateEscapeVelocity() << " km/s" << endl;
+    }
+
+    ~Planet() {
+        cout << "Planet " << name << " memory freed." << endl;
+    }
+};
+
+// Asteroid Class
 class Asteroid : public SpaceBody {
 public:
-    // Asteroid constructor, extracts data from the JSON object
+    Asteroid(const Asteroid& other)
+        : SpaceBody(other.name, other.diameter, other.mass),
+          id(other.id),
+          nasa_jpl_url(other.nasa_jpl_url),
+          absolute_magnitude(other.absolute_magnitude),
+          minDiameterKm(other.minDiameterKm),
+          maxDiameterKm(other.maxDiameterKm),
+          isDangerous(other.isDangerous),
+          closeApproachDate(other.closeApproachDate),
+          relativeVelocityKmPerS(other.relativeVelocityKmPerS),
+          missDistanceKm(other.missDistanceKm) {
+        cout << "Asteroid " << name << " copied." << endl;
+    }
+
     Asteroid(const json& asteroidData)
         : SpaceBody(
             asteroidData["name"],
@@ -73,21 +117,18 @@ public:
           id(asteroidData["id"]),
           nasa_jpl_url(asteroidData["nasa_jpl_url"]),
           absolute_magnitude(asteroidData["absolute_magnitude_h"]),
-          isDangerous(asteroidData["is_potentially_hazardous_asteroid"])
-    {
-        // Extract diameter information
+          isDangerous(asteroidData["is_potentially_hazardous_asteroid"]) {
         auto diameter = asteroidData["estimated_diameter"];
         minDiameterKm = diameter["kilometers"]["estimated_diameter_min"].get<double>();
         maxDiameterKm = diameter["kilometers"]["estimated_diameter_max"].get<double>();
 
-        // Extract close approach data
         auto close_approach = asteroidData["close_approach_data"][0];
         closeApproachDate = close_approach["close_approach_date"];
         relativeVelocityKmPerS = stod(close_approach["relative_velocity"]["kilometers_per_second"].get<string>());
         missDistanceKm = stod(close_approach["miss_distance"]["kilometers"].get<string>());
 
-        // Adjusted miss distance for visualization
         missDistanceKm = std::max(missDistanceKm / 2.0, EARTH_RADIUS * 2);
+        mass = calculateMass(asteroidData);
     }
 
     void printInfo() const override {
@@ -100,10 +141,11 @@ public:
         cout << "Close Approach Date: " << closeApproachDate << endl;
         cout << "Relative Velocity: " << relativeVelocityKmPerS << " km/s" << endl;
         cout << "Miss Distance: " << missDistanceKm << " km" << endl;
+        cout << "Mass: " << mass << " kg" << endl;
         cout << "Surface Gravity: " << calculateSurfaceGravity() << " m/s^2" << endl;
+        cout << "Impact Energy: " << calculateImpactEnergy() << " megatons of TNT" << endl;
     }
 
-    // Calculate impact energy in megatons of TNT
     double calculateImpactEnergy() const {
         double velocity_m_s = relativeVelocityKmPerS * 1000.0;
         double energy_joules = 0.5 * mass * pow(velocity_m_s, 2);
@@ -111,13 +153,22 @@ public:
         return energy_megatons;
     }
 
-    // Destructor
-    ~Asteroid() {
-        cout << "Asteroid " << name << " memory freed." << endl;
+    Asteroid operator+(const Asteroid& other) const {
+        Asteroid combinedAsteroid(*this);
+
+        combinedAsteroid.name = name + " & " + other.name;
+        combinedAsteroid.minDiameterKm += other.minDiameterKm;
+        combinedAsteroid.maxDiameterKm += other.maxDiameterKm;
+        combinedAsteroid.mass += other.mass;
+        combinedAsteroid.relativeVelocityKmPerS += other.relativeVelocityKmPerS;
+        combinedAsteroid.missDistanceKm += other.missDistanceKm;
+
+        combinedAsteroid.isDangerous = ((combinedAsteroid.minDiameterKm > 280) || (combinedAsteroid.relativeVelocityKmPerS > 5.0));
+        return combinedAsteroid;
     }
 
-    double getRelativeVelocityKmPerS() const {
-        return relativeVelocityKmPerS;
+    ~Asteroid() {
+        cout << "Asteroid " << name << " memory freed." << endl;
     }
 
 private:
@@ -131,69 +182,125 @@ private:
     double relativeVelocityKmPerS;
     double missDistanceKm;
 
-    // Helper function to calculate mass
     static double calculateMass(const json& asteroidData) {
         const double density = 3000.0;
         double diameterMin_m = asteroidData["estimated_diameter"]["kilometers"]["estimated_diameter_min"].get<double>() * 1000.0;
+        double diameterMax_m = asteroidData["estimated_diameter"]["kilometers"]["estimated_diameter_max"].get<double>() * 1000.0;
+
         double radiusMin = diameterMin_m / 2.0;
+        double radiusMax = diameterMax_m / 2.0;
+
         double volumeMin = (4.0 / 3.0) * M_PI * pow(radiusMin, 3);
-        return density * volumeMin;
+        double volumeMax = (4.0 / 3.0) * M_PI * pow(radiusMax, 3);
+
+        double avgVolume = (volumeMin + volumeMax) / 2.0;
+
+        double mass = density * avgVolume;
+        return mass;
     }
 };
 
-// Orbit visualization using SFML, includes Earth image
-void visualizeAsteroidOrbit(const Asteroid& asteroid) {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Asteroid Orbit Visualization");
+void handlePlanetOptions(Asteroid& asteroid) {
+    bool planetMenu = true;
+    while (planetMenu) {
+        cout << "\nPlease select an option:\n";
+        cout << "1. Display information on a planet.\n";
+        cout << "2. Estimate risk of damage if Asteroid Collides.\n";
+        cout << "3. Visualize the asteroid's orbit.\n";
+        cout << "4. Exit planet analysis.\n";
+        cout << "Enter your choice: ";
 
-    // Load Earth texture
-    sf::Texture earthTexture;
-    if (!earthTexture.loadFromFile("Earth_Image.jpeg")) {
-        cerr << "Failed to load Earth image!" << endl;
-        return;
-    }
+        int planetChoice;
+        cin >> planetChoice;
 
-    sf::Sprite earthSprite;
-    earthSprite.setTexture(earthTexture);
-    earthSprite.setOrigin(earthTexture.getSize().x / 2, earthTexture.getSize().y / 2);
-    earthSprite.setPosition(WINDOW_CENTER_X, WINDOW_CENTER_Y);
-    earthSprite.setScale(0.2f, 0.2f);
+        switch (planetChoice) {
+            case 1: {
+                for (size_t i = 0; i < predefinedPlanets.size(); ++i) {
+                    cout << i + 1 << ". " << predefinedPlanets[i].name << endl;
+                }
+                int planetSelection;
+                cin >> planetSelection;
 
-    // Create orbit circle
-    sf::CircleShape orbit;
-    orbit.setRadius(200);
-    orbit.setOrigin(orbit.getRadius(), orbit.getRadius());
-    orbit.setPosition(WINDOW_CENTER_X, WINDOW_CENTER_Y);
-    orbit.setOutlineColor(sf::Color::White);
-    orbit.setOutlineThickness(2);
-    orbit.setFillColor(sf::Color::Transparent);
+                if (planetSelection > 0 && planetSelection <= predefinedPlanets.size()) {
+                    const auto& planetData = predefinedPlanets[planetSelection - 1];
+                    Planet planet(planetData.name, planetData.diameter, planetData.mass);
+                    planet.printInfo();
+                } else {
+                    cout << "Invalid selection.\n";
+                }
+                break;
+            }
+            case 3: {
+                sf::RenderWindow window(sf::VideoMode(800, 800), "Asteroid Elliptical Orbit");
 
-    // Create asteroid shape
-    sf::CircleShape asteroidShape;
-    asteroidShape.setRadius(10);
-    asteroidShape.setFillColor(sf::Color::Green);
+                // Load Earth texture
+                sf::Texture earthTexture;
+                if (!earthTexture.loadFromFile("Earth_Image.jpeg")) {
+                    cerr << "Error loading Earth texture." << endl;
+                }
+                sf::Sprite earthSprite;
+                earthSprite.setTexture(earthTexture);
+                earthSprite.setPosition(WINDOW_CENTER_X - earthSprite.getLocalBounds().width / 2,
+                                        WINDOW_CENTER_Y - earthSprite.getLocalBounds().height / 2);
 
-    float angle = 0.0f;
-    float speed = asteroid.getRelativeVelocityKmPerS() * 0.05f;
+                // Slider for controlling the orbit position
+                sf::RectangleShape slider(sf::Vector2f(300, 10));
+                slider.setPosition(250, 700);
+                slider.setFillColor(sf::Color::White);
 
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
-                window.close();
+                sf::RectangleShape handle(sf::Vector2f(20, 30));
+                handle.setPosition(250, 690);
+                handle.setFillColor(sf::Color::Green);
+
+                // Ellipse orbit
+                float semiMajorAxis = 200.0f;  // X-axis length
+                float semiMinorAxis = 100.0f;  // Y-axis length
+
+                sf::CircleShape asteroidShape(10);  // Represent the asteroid
+                asteroidShape.setFillColor(sf::Color::Red);
+
+                while (window.isOpen()) {
+                    sf::Event event;
+                    while (window.pollEvent(event)) {
+                        if (event.type == sf::Event::Closed) window.close();
+                        if (event.type == sf::Event::MouseButtonPressed) {
+                            if (handle.getGlobalBounds().contains(window.mapPixelToCoords(sf::Mouse::getPosition(window)))) {
+                                draggingSlider = true;
+                            }
+                        }
+                        if (event.type == sf::Event::MouseButtonReleased) {
+                            draggingSlider = false;
+                        }
+                    }
+
+                    // Clear window
+                    window.clear();
+
+                    // Draw Earth
+                    window.draw(earthSprite);
+
+                    // Draw orbit (Ellipse logic)
+                    float angle = 2 * M_PI * timeElapsed;  // Use timeElapsed to get the angle
+                    float x = semiMajorAxis * cos(angle) + WINDOW_CENTER_X;
+                    float y = semiMinorAxis * sin(angle) + WINDOW_CENTER_Y;
+                    asteroidShape.setPosition(x - asteroidShape.getRadius(), y - asteroidShape.getRadius());
+
+                    // Draw asteroid
+                    window.draw(asteroidShape);
+
+                    // Draw the slider and handle
+                    drawSlider(window, slider, handle, timeElapsed);
+
+                    window.display();
+                }
+                break;
+            }
+            case 4:
+                planetMenu = false;
+                break;
+            default:
+                cout << "Invalid choice.\n";
         }
-
-        window.clear();
-
-        // Update asteroid position along the orbit
-        angle += speed;
-        float x = orbit.getPosition().x + orbit.getRadius() * std::cos(angle);
-        float y = orbit.getPosition().y + orbit.getRadius() * std::sin(angle);
-        asteroidShape.setPosition(x - asteroidShape.getRadius(), y - asteroidShape.getRadius());
-
-        window.draw(earthSprite);
-        window.draw(orbit);
-        window.draw(asteroidShape);
-        window.display();
     }
 }
 
@@ -217,14 +324,17 @@ int main() {
 
         json jsonData;
         json selectedNeoJson;
+
         string neo_data = fetch_neo_data(selectedDate, apiKey);
 
         if (neo_data.empty()) {
-            cerr << "Failed to fetch data from NASA API." << endl;
+            cerr << "Failed to fetch data from NASA API. Loading data from file..." << endl;
+
             if (!load_from_file(jsonData, "data.json")) {
                 cerr << "Failed to load data from file." << endl;
                 return 1;
             }
+
             selectedNeoJson = process_neo_data(jsonData, selectedDate);
         } else {
             try {
@@ -238,14 +348,17 @@ int main() {
 
         if (!selectedNeoJson.empty()) {
             try {
-                Asteroid asteroid(selectedNeoJson);
+                Asteroid asteroid1(selectedNeoJson);
 
                 bool asteroidMenu = true;
                 while (asteroidMenu) {
                     cout << "\nPlease select an option:\n";
                     cout << "1. Print all information about the asteroid.\n";
-                    cout << "2. Visualize the asteroid's orbit.\n";
-                    cout << "3. Exit.\n";
+                    cout << "2. Calculate and display the surface gravity.\n";
+                    cout << "3. Calculate and display the impact energy.\n";
+                    cout << "4. Combine this asteroid with another asteroid.\n";
+                    cout << "5. Analyze planets in the solar system.\n";
+                    cout << "6. Exit or Return to main menu.\n";
                     cout << "Enter your choice: ";
 
                     int choice;
@@ -253,21 +366,64 @@ int main() {
 
                     switch (choice) {
                         case 1:
-                            asteroid.printInfo();
+                            asteroid1.printInfo();
                             break;
                         case 2:
-                            visualizeAsteroidOrbit(asteroid);
+                            cout << "\nSurface Gravity: " << asteroid1.calculateSurfaceGravity() << " m/s^2\n";
                             break;
                         case 3:
+                            cout << "\nImpact Energy: " << asteroid1.calculateImpactEnergy() << " megatons of TNT\n";
+                            break;
+                        case 4: {
+                            asteroid1.printInfo();
+                            string selectedDate2;
+                            cout << "\nEnter a second date (YYYY-MM-DD) to search for NEOs: ";
+                            cin >> selectedDate2;
+                            string neo_data2 = fetch_neo_data(selectedDate2, apiKey);
+
+                            if (!neo_data2.empty()) {
+                                jsonData = json::parse(neo_data2);
+                                json selectedNeoJson2 = process_neo_data(jsonData, selectedDate2);
+
+                                if (!selectedNeoJson2.empty()) {
+                                    Asteroid asteroid2(selectedNeoJson2);
+                                    asteroid2.printInfo();
+
+                                    Asteroid combinedAsteroid = asteroid1 + asteroid2;
+                                    combinedAsteroid.printInfo();
+                                } else {
+                                    cout << "No asteroid selected for the second date.\n";
+                                }
+                            } else {
+                                cout << "Failed to fetch data for the second date from NASA API.\n";
+                            }
+                            break;
+                        }
+                        case 5:
+                            handlePlanetOptions(asteroid1);
+                            break;
+                        case 6:
                             asteroidMenu = false;
                             break;
                         default:
                             cout << "Invalid choice.\n";
                     }
+
+                    if (asteroidMenu) {
+                        cout << "\nDo you want to perform another action on this asteroid? (y/n): ";
+                        char continueChoice;
+                        cin >> continueChoice;
+                        if (continueChoice == 'n' || continueChoice == 'N') {
+                            asteroidMenu = false;
+                        }
+                    }
                 }
+
             } catch (const exception& e) {
-                cerr << "Error: " << e.what() << endl;
+                cerr << "Error creating Asteroid object: " << e.what() << endl;
             }
+        } else {
+            cout << "No asteroid selected.\n";
         }
 
         cout << "\nDo you want to analyze another asteroid? (y/n): ";
@@ -275,7 +431,7 @@ int main() {
         cin >> mainChoice;
         if (mainChoice == 'n' || mainChoice == 'N') {
             continueAnalyzing = false;
-            cout << "Exiting NEO Analyzer. Goodbye!\n";
+            cout << "Exiting the NEO Analyzer. Goodbye!\n";
         }
     }
 
